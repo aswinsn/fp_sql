0af3f9c72a624fdcd9247d416547f1f48a85b2cf
Aswin Singaram Natarajan <aswin.singaramnatarajan@csiro.au>
2020-11-05 08:04:24 +1100

    added node-notes to the output

--- fp_sql.R
+++ fp_sql.R
@@ -14,11 +14,15 @@ db <- dbConnect(sqlite.driver, "c:/Users/sin17m/Documents/Corrine/Backup tab 4 1
 
 db_list_tables(db)
 
+## Trait instance
+
 t_i_dat <- dbReadTable(db, "traitInstance")
 
 t_i_dat <- t_i_dat %>% 
   rename(traitInstance_id = X_id) %>% 
   select(traitInstance_id, trait_id)
+
+## Trait names
 
 trt_dat <- dbReadTable(db, "trait")
 
@@ -27,6 +31,8 @@ trt_dat <- trt_dat %>%
 
 t_i_dat <- t_i_dat %>% 
   left_join(trt_dat)
+
+## Datum value field
 
 raw_dat <- dbReadTable(db, "datum")
 
@@ -37,6 +43,16 @@ unit_id <- raw_dat %>%
 raw_dat <- raw_dat %>% 
   left_join(t_i_dat) %>% 
   select(node_id, value, trait_name, timestamp, userid)
+
+## fp Notes
+
+note_dat <- dbReadTable(db, "nodeNote")
+
+note_dat <- note_dat %>% 
+  select(node_id, note)
+
+raw_dat <- raw_dat %>% 
+  left_join(note_dat)
 
 ## Change tz below to appropriate timezone. Note Perth is different to other states 
 
@@ -67,16 +83,26 @@ dis_rr <- raw_dat %>%
   group_by(row, col) %>% 
   mutate(t_num = 1:n())
 
-dis_rr %>% 
+f_layout <- dis_rr %>% 
   filter(t_num == 2) %>% 
   ggplot(aes(row, col))+
-  geom_tile(colour = "black")+
-  geom_text(aes(label = variety))
+  geom_tile(fill = "gray",colour = "black")+
+  geom_text(aes(label = variety), size = 2.25)+
+  scale_x_continuous(breaks = 1:12, expand = c(0,0))+
+  scale_y_continuous(breaks = seq(0,80,5), expand = c(0,0))+
+  theme_bw()+
+  labs(caption = "2020 WA FP DATA - Tab 4")
+
+f_layout
+
+dir.create("plots")
+
+ggsave("plots/2020_tab_10_data.png", f_layout, units = "in", height = 9, width = 16)
 
 ## Change output filename to whatever suits you
 ## When you run it again make sure you change the name so that you are not overwriting
 
-write_csv(raw_dat, "output/fp_output.csv", na = "")
+write_csv(raw_dat, "output/fp_output_tab-04.csv", na = "")
 
 ## Clearing rstudio - optional!!
 
